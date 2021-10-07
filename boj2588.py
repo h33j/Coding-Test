242fc8d03ed06a9e40aee69866e3c49a561527d4
h33j <85170625+h33j@users.noreply.github.com>
2021-10-07 21:48:20 +0900

    Add files via upload
    boj 2588 곱셈

---
+++ boj2588.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+
+num3 = int(num2 / 100)
+num4 = int(num2 % 100 / 10)
+num5 = num2 % 10
+
+
+print(num1 * num5)
+print(num1 * num4)
+print(num1 * num3)
+print(num1 * num2)
